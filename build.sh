e9a6ae0aebf07176a4a6819f7636f33aa3a758ca
David Martínez García <davidmg103@hotmail.com>
2024-07-26 18:34:42 +0200

    Conan profile, C++23, frozen library for constexpr map

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf build
 
-conan install . --output-folder=build --build=missing
+conan install . --output-folder=build --build=missing -pr=./conan_profile
 cd build
 cmake .. -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake" -DCMAKE_BUILD_TYPE="Release"
 cmake --build .
--- src/interval_map.cpp
+++ src/interval_map.cpp
@@ -3,15 +3,19 @@
 #include <string_view>
 #include <print>
 #include "interval_map.hpp"
+#include "frozen/map.h"
 
 int main()
 {
+  constexpr frozen::map<int, int, 1> map { {0,0} };
+
   auto even = [](int i) { return i & 1; };
 
   std::vector<int> v {1,2,3,4,5,6,7};
   constexpr farra::interval_map<std::string_view, int> im{};
   
-  //auto filtered = v | std::views::filter(even);
+  auto filtered = v | std::views::filter(even);
 
+  std::print("{}", 5 * 4);
   return 0;
 }
