07ee1ef1d8b0058ba60e5718f852eaa32a729baf
David Martínez García <davidmg103@hotmail.com>
2024-07-27 16:50:27 +0200

    We have a tiny constexpr upper bound with ranges
    We learnt something about constexpr that is awesome

--- src/interval_map.cpp
+++ src/interval_map.cpp
@@ -1,21 +1,40 @@
-#include <ranges>
-#include <vector>
-#include <string_view>
 #include <print>
+#include <string_view>
+#include <map>
+
 #include "interval_map.hpp"
-#include "frozen/map.h"
+
+template<typename T>
+void printContainer(T& container)
+{
+  for(const auto& item : container)
+    std::println("{}, {}", item.first, item.second);
+}
 
 int main()
 {
-  constexpr frozen::map<int, int, 1> map { {0,0} };
+  // constexpr farra::interval_map<uint8_t, float, 5> im = 
+  // {
+  //   {1, 1.1f},
+  //   {5, 5.1f},
+  //   {10, 10.1f},
+  //   {15, 15.1f},
+  //   {20, 20.1f},
+  // };
 
-  auto even = [](int i) { return i & 1; };
+  constexpr farra::map<int,int, 2> map = { {1,5}, {2,10} };
+  //map.upper_bound(2);
 
-  std::vector<int> v {1,2,3,4,5,6,7};
-  constexpr farra::interval_map<std::string_view, int> im{};
-  
-  auto filtered = v | std::views::filter(even);
 
-  std::print("{}", 5 * 4);
-  return 0;
+  //std::array<std::pair<int, int>, 2> a { std::pair{1,1}, {2,2}};
+
+  //auto is_greater = [](auto arrayKey) { return true; };
+  //auto it = std::ranges::find_if(a, is_greater);
+
+  //static_assert(map.cbegin()->second == 5);
+  static_assert(map.upper_bound(1)->second == 10);
+
+  //std::print("{}", map.upper_bound(4)->second);
+
+  //std::array<std::pair<int, int>, 2> array { std::pair{1,5}, {2,10} };
 }
--- src/interval_map.hpp
+++ src/interval_map.hpp
@@ -1,9 +1,47 @@
+#include <array>
+#include <ranges>
+#include <algorithm>
+#include <concepts>
+
+template<typename T>
+concept has_operator_less = requires(T&& type) { T{} < T{}; };
+
 namespace farra
 {
-    template<typename Key_t, typename Value_t>
-    class interval_map
+    template<typename _Key_t, typename _Value_t, std::size_t N>
+    requires has_operator_less<_Key_t>
+    class map
     {
     public:
-        constexpr interval_map() {}
+        using key_type      = _Key_t;
+        using mapped_type   = _Value_t;
+        using value_type    = std::pair<_Key_t, _Value_t>;
+        
+        using const_pointer     = const value_type*;
+        //TODO: Not sure about this
+        using const_iterator    = const_pointer;
+         
+        constexpr map(const std::initializer_list<value_type>& entries)
+        {
+            std::copy(entries.begin(), entries.end(), container_.begin());
+        }
+
+        //TODO: Const correctness with C++23
+
+        [[nodiscard]] constexpr const_iterator upper_bound(_Key_t key) const
+        {
+            //!!: Parameters are never gonna be constexpr, so assuming that a parameter is constexpr will make the compiler to fail. We should not assume that and just remove the constexpr, just mark the function as constexpr and if the compiler is able to do that the execution will be compilation time. constexpr auto is_greater is bad!! Explain in obsidian 
+
+            auto is_greater = [key](auto arrayKey) { return arrayKey.first > key; };
+            return std::ranges::find_if(container_, is_greater);
+        }
+
+        constexpr const_iterator cbegin() const { return container_.cbegin(); }
+        constexpr const_iterator cend() const { return container_.cend(); }
+
+    private:
+        //It doesn't make a lot of sense to have an array what about look up or something like that in case the map wants to be used as a normal map?
+        using Container_t = std::array<std::pair<_Key_t, _Value_t>, N>;
+        Container_t container_{};
     };
 }
