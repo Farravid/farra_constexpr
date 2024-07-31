80a52501d9138bf0c331e72afba3820fd5e932ee
David Martínez García <davidmg103@hotmail.com>
2024-07-31 19:37:40 +0200

    Build script for base, benchmark, test, all

--- build.sh
+++ build.sh
@@ -1,11 +1,34 @@
+target_binary=--target=interval_map
+target_name=interval_map
+
+target_interval=
+target_interval_test=
+target_interval_benchmark=
+
+if [[ "$1" == "benchmark" ]];
+then
+    target_binary=$target_binary"_benchmark"
+    target_interval_benchmark=./interval_map_benchmark
+elif [[ "$1" == "test" ]];
+then
+    target_binary=$target_binary"_test"
+    target_interval_test=./interval_map_test
+elif [[ "$1" == "all" ]];
+then
+    target_binary=
+    target_interval_benchmark=./interval_map_benchmark
+    target_interval_test=./interval_map_test
+    target_interval=./interval_map
+else
+    target_interval=./interval_map
+fi
+
 conan install . --build=missing -pr=./conan_profile
 cd build/Release
 source ./generators/conanbuild.sh
 cmake ../.. -DCMAKE_TOOLCHAIN_FILE="generators/conan_toolchain.cmake" -DCMAKE_BUILD_TYPE="Release"
-cmake --build . --target=interval_map
-./interval_map
+cmake --build . $target_binary
 
-# See a way to launch only test, benchmark, all...
-#./interval_map_test
-
-#./interval_map_benchmark
+$target_interval
+$target_interval_test
+$target_interval_benchmark
